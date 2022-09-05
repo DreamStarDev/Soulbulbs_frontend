9d1766e8c5777c8f1b644f7a84b60c30e6ae442d
SuperDev <yuri01051@outlook.com>
2022-09-05 18:16:04 +1000

    9_5_2

--- src/Pages/Details.js
+++ src/Pages/Details.js
@@ -9,10 +9,15 @@ import WalletConnectModal from "../Components/WalletConnectModal";
 
 function Details() {
     const appContext = useAppContext();
+    const [category, setCategory] = useState(0);
 
     useEffect(() => {
         appContext.generateImage();
     });
+
+    const selectCategory = (id) => {
+        setCategory(id);
+    }
 
     return (
         <div style={{ overflowY: "scroll" }} >
@@ -32,13 +37,63 @@ function Details() {
                                 <div className="color-boxs-head ">
                                     <h4 className='text-dark'>Simply click to add your favourite trait</h4>
                                 </div>
+                                <div className="row" style={{ paddingBottom: 30 }}>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(0)}>
+                                        <img 
+                                            src={category != 0 ? require('../images/Icons/background.svg').default : require('../images/Icons/background_highlighted.svg').default} 
+                                            alt="Background" 
+                                        />
+                                    </div>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(1)}>
+                                        <img 
+                                            src={category != 1 ? require('../images/Icons/torso.svg').default : require('../images/Icons/torso_highlighted.svg').default} 
+                                            alt="Torso" 
+                                        />
+                                    </div>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(2)}>
+                                        <img 
+                                            src={category != 2 ? require('../images/Icons/bulb.svg').default : require('../images/Icons/bulb_highlighted.svg').default} 
+                                            alt="Bulb" 
+                                        />
+                                    </div>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(3)}>
+                                        <img 
+                                            src={category != 3 ? require('../images/Icons/el_glasses.svg').default : require('../images/Icons/glasses_highlighted.svg').default} 
+                                            alt="Glasses" 
+                                        />
+                                    </div>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(4)}>
+                                        <img 
+                                            src={category != 4 ? require('../images/Icons/hat.svg').default : require('../images/Icons/hat_highlighted.svg').default } 
+                                            alt="Hat" 
+                                        />
+                                    </div>
+                                    <div className="col-md-2 col-sm-4" onClick={() => selectCategory(5)}>
+                                        <img 
+                                            src={category != 5 ? require('../images/Icons/overhead.svg').default : require('../images/Icons/overhead_highlighted.svg').default} 
+                                            alt="Overhead" 
+                                        />
+                                    </div>
+                                </div>
                                 <div className="row cardsAllmaim">
-                                    <TraitCollection trait='background' />
-                                    <TraitCollection trait='body' />
-                                    <TraitCollection trait='bulb' />
-                                    <TraitCollection trait='glasses' />
-                                    <TraitCollection trait='hat' />
-                                    <TraitCollection trait='overhead' />
+                                    {
+                                        category == 0 ? <TraitCollection trait='background' /> : ''
+                                    }
+                                    {
+                                        category == 1 ? <TraitCollection trait='body' /> : ''
+                                    }
+                                    {
+                                        category == 2 ? <TraitCollection trait='bulb' /> : ''
+                                    }
+                                    {
+                                        category == 3 ? <TraitCollection trait='glasses' /> : ''
+                                    }
+                                    {
+                                        category == 4 ? <TraitCollection trait='hat' /> : ''
+                                    }
+                                    {
+                                        category == 5 ? <TraitCollection trait='overhead' /> : ''
+                                    }
                                 </div>
                             </div>
                             <div className="details-box">
